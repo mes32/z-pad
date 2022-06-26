364211cbe54d3f1ad4a819edfb0986765fb2e24c
mes32 <stockman.mike@gmail.com>
2022-06-26 15:27:44 -0500

    use a div and child spans to mimic a textarea

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,36 @@
-let text = 'HELLO WORLD!'
+let text = 'HELLO WORLD!\n'
 
 function setValue(newValue) {
-  const textArea = document.getElementById('textarea');
-  textArea.value = newValue;
+  const textArea = document.getElementById('textarea')
+
+  for (let i = 0, c = ''; c = newValue.charAt(i); i++) {
+    if (c === '\n') {
+      const newLine = document.createElement('br')
+      textArea.appendChild(newLine)
+    } else {
+      const span = document.createElement('span')
+      span.innerHTML = c
+      textArea.appendChild(span)
+    }
+  }
+}
+
+function removeValue() {
+  const textArea = document.getElementById('textarea')
+  textArea.removeChild(textArea.lastChild)
+}
+
+function logKey(e) {
+  if (e.code === 'Enter') {
+    setValue('\n')
+  } else if (e.code === 'Backspace') {
+    removeValue()
+  } else {
+    setValue(e.key.toUpperCase())
+  }
 }
 
 window.onload = function() {
   setValue(text)
+  document.addEventListener('keydown', logKey)
 }
